b2c148c603e26e964c65a9c911b987b2d0656129
Antonio Insuasti <efainsua@pichincha.com>
2023-01-06 15:49:24 -0500

    add logger

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,26 +1,46 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { readFile, writeFile } from "fs/promises";
+
 
 @Injectable()
 export class MessageRepository {
+    private readonly logger = new Logger(MessageRepository.name);
 
     async findOne(id: number) {
-        const contents = await readFile("messages.json", "utf-8");
-        const messages = JSON.parse(contents);
-        return messages[id];
+        try {
+            const contents = await readFile("messages.json", "utf-8");
+            const messages = JSON.parse(contents);
+            this.logger.verbose(`Found message with id ${id}`);
+            return messages[id];    
+        } catch (error) {
+            this.logger.error(error);
+        }
+        
     }
 
     async findAll() {
-        const contents = await readFile("messages.json", "utf-8");
-        const messages = JSON.parse(contents);
-        return messages;
+        try {
+            const contents = await readFile("messages.json", "utf-8");
+            const messages = JSON.parse(contents);
+            this.logger.verbose(`Found all messages`);
+            return messages;    
+        } catch (error) {
+            this.logger.error(error);
+        }
+        
     }
 
     async create(content: string) {
-        const contents = await readFile("messages.json", "utf-8");
-        const messages = JSON.parse(contents);
-        const id = Math.floor(Math.random() * 999);
-        messages[id] = { id, content };
-        await writeFile("messages.json", JSON.stringify(messages));
+        try {
+            const contents = await readFile("messages.json", "utf-8");
+            const messages = JSON.parse(contents);
+            const id = Math.floor(Math.random() * 999);
+            messages[id] = { id, content };
+            await writeFile("messages.json", JSON.stringify(messages));
+            this.logger.verbose(`Created message with id ${id}`);    
+        } catch (error) {
+            this.logger.error(error);
+        }
+        
     }
 }
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,20 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { MessageRepository } from "./messages.repository";
 
 @Injectable()
 export class MessageServices {
-
+    public readonly logger = new Logger(MessageServices.name);
     constructor(public messagesRepo: MessageRepository) {}
 
     findOne(id: number) {
+        this.logger.verbose(`Getting message with id ${id}`);
         return this.messagesRepo.findOne(id);
     }
 
     findAll() {
+        this.logger.verbose(`Getting all messages`);
         return this.messagesRepo.findAll();
     }
 
     create(content: string) {
+        this.logger.verbose(`Creating message with content ${content}`);
         return this.messagesRepo.create(content);
     }
     
